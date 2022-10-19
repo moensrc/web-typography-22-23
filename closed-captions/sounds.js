d438ab345cfdd69187d00ebce286464790d9b54b
moensrc <rcmoens.rm@gmail.com>
2022-10-19 17:09:21 +0200

    Added sounds to sounds.js array from part 2

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -11,5 +11,26 @@ sounds = [
 	31,
 	32,
 	34,
-	86
+	86,
+	87,
+	103, 
+	105,
+	117
 ];
+
+//sound0 0 = intro sound fragment 1
+//sound1 9 = alarm
+//sound2 12 = siren
+//sound3 15 = build-up
+//sound4 16.5 = peep
+//sound5 24 = build-up
+//sound6 25.8 = peep
+//sound7 31 = build-up
+//sound8 32 = peep
+//sound9 34 = long beep in cells-scene
+//sound10 86 = intro sound fragment 2
+//sound11 87 = long beeping
+//sound12 103 = beep beep notification
+//sound13 105 = louder build-up from sound 10
+//sound14 117 = increasing overlapped sound
+//sound15 130 = beeping stops, 
